97e01d23d5c6849bf7a550cb67711f0e0662f8b1
avemunoori <85594271+avemunoori@users.noreply.github.com>
2025-07-12 18:04:13 -0400

    Add database connection logging to diagnose data persistence issues

--- src/main/java/com/ascend/AscendApiApplication.java
+++ src/main/java/com/ascend/AscendApiApplication.java
@@ -4,14 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableRetry
-public class AscendApiApplication {
+public class AscendApiApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(AscendApiApplication.class);
+
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AscendApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("=== DATABASE CONNECTION INFO ===");
+		logger.info("Database URL: {}", dataSource.getConnection().getMetaData().getURL());
+		logger.info("Database Product: {}", dataSource.getConnection().getMetaData().getDatabaseProductName());
+		logger.info("Database Version: {}", dataSource.getConnection().getMetaData().getDatabaseProductVersion());
+		logger.info("================================");
 	}
 
 }
